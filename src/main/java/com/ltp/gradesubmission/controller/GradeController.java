5eb3da5390362cb2ef19401d5691f48848341ff7
Nitzan <nitzan16001@gmail.com>
2023-05-14 02:14:17 +0300

    Added Beans and Dependency Injection

--- src/main/java/com/ltp/gradesubmission/controller/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/controller/GradeController.java
@@ -1,5 +1,6 @@
 package com.ltp.gradesubmission.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,7 +16,8 @@ import jakarta.validation.Valid;
 @Controller
 public class GradeController {
 
-    GradeService gradeService = new GradeService();
+    @Autowired 
+    GradeService gradeService;
 
     @GetMapping("/")
     public String getForm(Model model, @RequestParam(required = false) String id) {
